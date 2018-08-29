e0757ae029d30844c488f59457b15d058fd17083
EBINAtettyan <tetsuya.ebina1016@gmail.com>
2018-08-29 21:23:20 +0900

    ルーブリックの値をDBに登録する機能完成

--- WEB-INF/src/dao/ResponseDataDAO.java
+++ WEB-INF/src/dao/ResponseDataDAO.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import com.mysql.jdbc.PreparedStatement;
+
 import beans.ResponseData;
 import utility.DriverAccessor;
 
@@ -24,9 +26,11 @@ public class ResponseDataDAO extends DriverAccessor {
 			ArrayList<ResponseData> responseDataList = new ArrayList<ResponseData>();
 
 			while (rs.next()) {
-			ResponseData responseData = new ResponseData(rs.getInt("id"),rs.getInt("user_id"),rs.getInt("lesson_id"),rs.getInt("per1"),rs.getInt("per2"),rs.getInt("per3"),rs.getInt("per4"));
-			responseDataList.add(responseData);
-			System.out.println(responseData.getLessonId());
+				ResponseData responseData = new ResponseData(rs.getInt("id"), rs.getInt("user_id"),
+						rs.getInt("lesson_id"), rs.getInt("per1"), rs.getInt("per2"), rs.getInt("per3"),
+						rs.getInt("per4"));
+				responseDataList.add(responseData);
+				System.out.println(responseData.getLessonId());
 			}
 			stmt.close();
 			rs.close();
@@ -46,4 +50,41 @@ public class ResponseDataDAO extends DriverAccessor {
 		{
 		}
 	}
+
+	public void registResponseData(ResponseData responseData) {
+
+		//コネクションの接続開始
+		Connection con = null;
+		con = createConnection();
+
+		try {
+
+			//insert文の宣言（例えば、responseData.getUserId()）
+			String sql = "insert into response_data(id,user_id,lesson_id,per1,per2,per3,per4) values(?, ?, ?, ?, ?,?,?)";
+
+			//PreparedStatementの宣言開始（DBに格納するための変数の宣言）
+			PreparedStatement stmt = (PreparedStatement) con.prepareStatement(sql);
+			stmt.setInt(1, 0);
+			stmt.setInt(2, responseData.getUserId());
+			stmt.setInt(3, responseData.getLessonId());
+			stmt.setInt(4, responseData.getPer1());
+			stmt.setInt(5, responseData.getPer2());
+			stmt.setInt(6, responseData.getPer3());
+			stmt.setInt(7, responseData.getPer4());
+
+			//DBのUpdate
+			stmt.executeUpdate();
+
+			//PreparedStatementの終了
+			stmt.close();
+
+		} catch (
+
+		SQLException e) {
+			e.printStackTrace();
+			return;
+		} finally {
+		}
+	}
+
 }
