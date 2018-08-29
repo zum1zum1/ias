7aecff7f14ea9c6ab9eae39a600e4b573794c868
zum1zum1 <e155403n@st.u-gakugei.ac.jp>
2018-08-29 21:45:47 +0900

    ヒントの追加

--- WEB-INF/src/dao/ResponseDataDAO.java
+++ WEB-INF/src/dao/ResponseDataDAO.java
@@ -12,30 +12,46 @@ import beans.ResponseData;
 import utility.DriverAccessor;
 
 public class ResponseDataDAO extends DriverAccessor {
-	// ユーザIDからパスワードを返す
+	// ユーザーのidからそのユーザーが記録した反応データを全て返すメソッド
 	public ArrayList<ResponseData> selectResponseData(int userId) {
-
+		// データベースと繋げるおまじない
+		// Connectionを宣言する
 		Connection con = null;
+		// コネクションを作る
 		con = createConnection();
-
 		try {
+
+			// sql文を書く   引数として持ってきたユーザーのidと一致するuser_idのレコードをresponse_dataテーブルからSELECTしてくる。必要に応じてorder by ooo descとかで並べ替える必要があるかもしれない。。。
 			String sql = "select * from response_data where user_id = '" + userId + "' order by lesson_id desc;";
 
+
+
+			// Statementの宣言
 			Statement stmt = con.createStatement();
+			// ResultSetの宣言とsqlの実行
 			ResultSet rs = stmt.executeQuery(sql);
+			// 反応データのArrayListを宣言する
 			ArrayList<ResponseData> responseDataList = new ArrayList<ResponseData>();
 
+
+			//一行ごとにArrayListに検索結果を格納する(while文)
 			while (rs.next()) {
 				ResponseData responseData = new ResponseData(rs.getInt("id"), rs.getInt("user_id"),
 						rs.getInt("lesson_id"), rs.getInt("per1"), rs.getInt("per2"), rs.getInt("per3"),
 						rs.getInt("per4"));
 				responseDataList.add(responseData);
-				System.out.println(responseData.getLessonId());
 			}
+
+
+			// Statementを閉じる
 			stmt.close();
+			// ResultSetを閉じる
 			rs.close();
+			// コネクションを閉じる
 			closeConnection(con);
 
+
+			// 反応データのArrayListを返す
 			return responseDataList;
 
 		} catch (
