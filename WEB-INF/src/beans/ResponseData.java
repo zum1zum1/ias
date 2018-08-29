9538bd56bc7992702cc7aa12721275457c8261c6
zum1zum1 <e155403n@st.u-gakugei.ac.jp>
2018-08-29 20:35:52 +0900

    beansの修正

--- WEB-INF/src/beans/ResponseData.java
+++ WEB-INF/src/beans/ResponseData.java
@@ -3,18 +3,18 @@ package beans;
 public class ResponseData {
 
 	private int id = 0;
-	private int user_id = 0;
-	private int lesson_id = 0;
+	private int userId = 0;
+	private int lessonId = 0;
 	private int per1 = 0;
 	private int per2 = 0;
 	private int per3 = 0;
 	private int per4 = 0;
 
-	public ResponseData(int id,int user_id,int lesson_id,int per1,int per2,int per3,int per4){
+	public ResponseData(int id,int userId,int lessonId,int per1,int per2,int per3,int per4){
 		super();
 		this.id = id;
-		this.user_id = user_id;
-		this.lesson_id = lesson_id;
+		this.userId = userId;
+		this.lessonId = lessonId;
 		this.per1 = per1;
 		this.per2 = per2;
 		this.per3 = per3;
@@ -26,57 +26,71 @@ public class ResponseData {
 		// TODO 自動生成されたコンストラクター・スタブ
 	}
 
+
 	public int getId() {
 		return id;
 	}
+
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-	public int getUser_id() {
-		return user_id;
+
+	public int getUserId() {
+		return userId;
 	}
 
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
+
+	public void setUserId(int userId) {
+		this.userId = userId;
 	}
 
-	public int getLesson_id() {
-		return lesson_id;
+
+	public int getLessonId() {
+		return lessonId;
 	}
 
-	public void setLesson_id(int lesson_id) {
-		this.lesson_id = lesson_id;
+
+	public void setLessonId(int lessonId) {
+		this.lessonId = lessonId;
 	}
+
 
 	public int getPer1() {
 		return per1;
 	}
 
+
 	public void setPer1(int per1) {
 		this.per1 = per1;
 	}
+
 
 	public int getPer2() {
 		return per2;
 	}
 
+
 	public void setPer2(int per2) {
 		this.per2 = per2;
 	}
+
 
 	public int getPer3() {
 		return per3;
 	}
 
+
 	public void setPer3(int per3) {
 		this.per3 = per3;
 	}
 
+
 	public int getPer4() {
 		return per4;
 	}
+
 
 	public void setPer4(int per4) {
 		this.per4 = per4;
